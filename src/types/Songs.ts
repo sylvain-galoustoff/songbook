cac79f6fe174a77518d9bdc27cd435486f9f65ba
sylvain <sylvain.galoustoff@gmail.com>
2025-12-26 00:29:43 +0100

    Home display songs from db

---
+++ src/types/Songs.ts
@@ -0,0 +1,7 @@
+export interface Song {
+  id: string;
+  title: string;
+  fileUrl: string;
+  fileName: string;
+  createdAt?: any;
+}
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,19 +3,45 @@ import Header from "../../components/Header/Header";
 import SongLine from "../../components/SongLine/SongLine";
 import styles from "./Home.module.css";
 import { IoAddCircle } from "react-icons/io5";
+import { useEffect, useState } from "react";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { db } from "../../firebase";
+import type { Song } from "../../types/Songs";
 
 export default function Home() {
+  const [songs, setSongs] = useState<Song[]>([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchSongs = async () => {
+      try {
+        const q = query(collection(db, "songs"), orderBy("title", "asc"));
+
+        const snapshot = await getDocs(q);
+
+        const songsData: Song[] = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...(doc.data() as Omit<Song, "id">),
+        }));
+
+        setSongs(songsData);
+      } catch (error) {
+        console.error("Erreur chargement songs :", error);
+      }
+    };
+
+    fetchSongs();
+  }, []);
+
+  const renderSongsLines = songs.map((song) => (
+    <SongLine key={song.id} title={song.title} />
+  ));
 
   return (
     <div className={styles.home} id="home">
       <Header />
 
-      <main className={styles.main}>
-        <SongLine title="Evolution" />
-        <SongLine title="C'est maladif" />
-        <SongLine title="Les vieillards" />
-      </main>
+      <main className={styles.main}>{renderSongsLines}</main>
 
       <footer>
         <button
